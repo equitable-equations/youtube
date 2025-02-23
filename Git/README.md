c874893acd56c48dce9cb13670514b359f8446af
equitable-equations <agard@lakeforest.edu>
2025-02-23 11:50:43 -0600

    Add git for R users

---
+++ Git/README.md
@@ -0,0 +1,4 @@
+# Git for R users
+[<img src="git thumb.png" align="right" height="100" />](<https://youtu.be/njMEXGfUQG8>)
+
+Hi everybody! In this folder, you'll find materials supporting my vid, **Git for R users**, which you can find at <https://youtu.be/njMEXGfUQG8>. 
---
+++ Git/git for R users.R
@@ -0,0 +1,10 @@
+# First install git.
+# Mac: Enter `git config` in the terminal
+# Windows: https://gitforwindows.org/
+
+
+# Setup -------------------------------------------------------------------
+
+library(usethis)
+use_git_config(user.name = "Your Name", 
+               user.email = "your@email.com")
